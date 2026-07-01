8a0fea5a3eaab3209c6ecb36c44b0335f873de35
AnastasiiaAbr <zalogova.an@yandex.ru>
2026-07-01 22:25:19 +0500

    added React Router and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Positions from "./pages/Positions";
+import CVs from "./pages/CVs";
+import NotFound from "./pages/NotFound";
+
 function App() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/positions' element={<Positions />} />
+        <Route path='/cvs' element={<CVs />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/pages/CVs.jsx
@@ -0,0 +1,3 @@
+export default function CVs() {
+  return <h1>CVs</h1>;
+}
