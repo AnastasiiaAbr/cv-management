ca45bf38f7cffe652d7abe8a7a3c32ee67e3e29a
AnastasiiaAbr <zalogova.an@yandex.ru>
2026-07-09 19:44:26 +0500

    user registration with Prisma and PostgreSQL

--- backend/src/controllers/auth-controller.js
+++ backend/src/controllers/auth-controller.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import prisma from '../prisma/prisma.js';
 
 export const test = (req, res) => {
   res.status(200).json({
@@ -7,17 +8,44 @@ export const test = (req, res) => {
 };
 
 export const register = async (req, res) => {
-  const {email, password} = req.body;
+  try {
+    const { email, password } = req.body;
 
-  if (!email || !password) {
-    return res.status(400).json({
-      message: "Email and password are required.",
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required.",
+      });
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email,
+      },
     });
-  };
 
-  const hashedPassword = await bcrypt.hash(password, 10);
+    if (existingUser) {
+      return res.status(409).json({
+        message: "User already exists.",
+      });
+    }
 
-  res.status(201).json({
-    message: 'User registered successfully',
-  })
-}
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return res.status(201).json({
+      message: "User registered successfully.",
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      message: "Internal server error.",
+    });
+  }
+};
